d7b78757f8e0360c5d9e70a3e5c203b60c427e19
Erdenejargal MunkhErdene <76554232+Erdenejargal-dev@users.noreply.github.com>
2025-07-09 19:49:48 +0800

    Update page.jsx

--- src/app/creator-dashboard/page.jsx
+++ src/app/creator-dashboard/page.jsx
@@ -7,7 +7,7 @@ import {
   TrendingUp, Calendar, User
 } from 'lucide-react';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL + '/api';
 
 const CreatorDashboard = () => {
   const [activeTab, setActiveTab] = useState('overview');
