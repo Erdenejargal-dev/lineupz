bf26697037cff6527ecb16c728af00630810292e
Erdenejargal MunkhErdene <76554232+Erdenejargal-dev@users.noreply.github.com>
2025-07-09 19:19:21 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen bg-white dark:bg-gray-900 flex flex-col">
       <main className="flex-1 flex flex-col justify-center items-center text-center px-4">
         <div className="mb-8">
           <SplitText className="text-5xl tracking-tighter font-medium">
