444036e37c2fda09f134b03767f2c29a144c6478
Erdenejargal MunkhErdene <76554232+Erdenejargal-dev@users.noreply.github.com>
2025-08-05 20:43:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.tabi.mn/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
